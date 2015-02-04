c60c112814144ecbb26c27c48bb2e4193fce729f
yungtechboy1 <yungtechboy1@gmail.com>
2015-02-04 12:06:50 -0500

    Major Bug Fix On Death
    BMP3 #Time 4h 13m #comment Man that was hard!!

--- src/CyberTech/Main.php
+++ src/CyberTech/Main.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * KillBounty (v1.0.1.0) by CyberTech++
+ * KillBounty (v1.0.1.1) by CyberTech++
  * Developer: CyeberTech++ (Yungtechboy1)
  * Website: http://www.cybertechpp.com
  * Date: 2/3/2015 11:47 PM (UTC)
@@ -10,13 +10,32 @@
 namespace CyberTech;
 
 use pocketmine\Player;
+//use pocketmine\Server;
 use pocketmine\event\Listener;
+/*use pocketmine\event\player\PlayerEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;*/
 use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\entity\EntityDeathEvent;
+/*use pocketmine\event\entity\EntityLevelChangeEvent;
+use pocketmine\level\Position;
+use pocketmine\math\Vector3;
+use pocketmine\permission\Permission;
+use pocketmine\permission\PermissionAttachment;*/
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
+//use pocketmine\utils\TextFormat;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use onebone\economyapi\EconomyAPI;
+//use pocketmine\event\player\PlayerQuitEvent;
+/*use pocketmine\utils\Config;
+use pocketmine\plugin\PluginBase;
+;
+use pocketmine\Player;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\player\PlayerJoinEvent;*/
 
 class Main extends PluginBase implements Listener{
     
@@ -27,6 +46,7 @@ class Main extends PluginBase implements Listener{
          @mkdir($this->getDataFolder());
          $this->getLogger()->info("Boutny Plugin Has Been Enabled!");
          $this->loadYml();
+         //$this->getServer()->getPluginManager()->registerEvents(new Main($this), $this);
          $this->db = new \SQLite3($this->getDataFolder() . "Boutny.db");
          $this->db->exec("CREATE TABLE IF NOT EXISTS bounty (id INTEGER PRIMARY KEY AUTOINCREMENT, player TEXT, amount INTEGER, setby TEXT);");
          $this->db->exec("CREATE TABLE IF NOT EXISTS settings (id INTEGER PRIMARY KEY AUTOINCREMENT , name TEXT, val TEXT);");
@@ -39,6 +59,9 @@ class Main extends PluginBase implements Listener{
         switch($command->getName()){
 			case "bounty":
                             if ($args[0] === "set" && isset($args[1]) && isset($args[2])){
+                                //Set Bounty
+                                //Command Ex
+                                //bounty set yungtech 1000
                                 $this->getLogger()->info("Boutny Plugin Has Been Set Command!");
                                 $this->SetBounty($sender, $args[1] ,$args);
                             }elseif($args[0] === "del"){
@@ -70,6 +93,7 @@ class Main extends PluginBase implements Listener{
             $endnum = $startnum - 5;
             for ($x = $endnum; $x<$startnum; $x++){
             $xx = $x + 1;
+            //$sender->sendMessage($x);
             $sqlr = $this->db->query("SELECT * FROM bounty ORDER BY `id` DESC LIMIT $x,1");
             $eslf = $sqlr->fetchArray(SQLITE3_ASSOC);
             $sender->sendMessage("#".$xx."-".$eslf['player']." -> $". $eslf['amount']);
@@ -80,13 +104,16 @@ class Main extends PluginBase implements Listener{
         }
 
         public function DeleteBounty(Player $sender, $player) {
+            //Delets Bounty On That Player
             $yml = (new Config($this->getServer()->getDataPath() . "/plugins/Bounty/" . "Bounty.yml", Config::YAML ,array()));
             $temp = $yml->getAll();
             $sendern = $sender->getName();
+            // TODO - ADD MULTIPLE CLAUSE
             $sqlr = $this->db>query("SELECT * FROM bounty WHERE setby='$sendern'AND player LIKE '%$player%' ORDER BY `id` DESC LIMIT 0,1");
             $eslf = $sqlr->fetchArray(SQLITE3_ASSOC);
             $this->api->addMoney ( $sender->getName(), $eslf['amount'] );
             $message = "Bounty Removed From ".$eslf['player']."'s Head.";
+            //$player->sendMessage($message);
             $this->getServer()->broadcastMessage($message);
         }
         
@@ -101,12 +128,20 @@ class Main extends PluginBase implements Listener{
                     $this->api->reduceMoney($sendplayer->getName(), $args[2]);
                     $player1 = $sendplayer->getName();
                     $player2 = $setplayer;
+                    //$a1 = $args[1];
                     $a2 = $args[2];
+                    //$a3 = $args[3];
                     $stmt = $this->db->prepare("INSERT OR REPLACE INTO bounty (player, amount, setby) VALUES (:bounty, :amount, :setby);");
                     $stmt->bindValue(":bounty", $setplayer);
                     $stmt->bindValue(":amount", $a2);
                     $stmt->bindValue(":setby", $sendplayer->getName());
                     $result = $stmt->execute();
+                    /*
+                    $temp['Current-Bounties'][$player2] = array();
+                    $temp['Current-Bounties'][$player2]['bounty'] = $a2;
+                    $temp['Current-Bounties'][$player2]['set-by'] = $player1;
+                    $yml->setAll($temp);
+                    $yml->save();*/
                     $m = $player1." Has set a bounty of ". $a2 . " On " . $player2."'s Head!";
                     $this->getServer()->broadcastMessage($m);
                     return true;
@@ -130,24 +165,34 @@ class Main extends PluginBase implements Listener{
             $yml = (new Config($this->getServer()->getDataPath() . "/plugins/Bounty/" . "Bounty.yml", Config::YAML ,array()));
             $temp = $yml->getAll();
             $player = $death->getEntity();
-            $killer = $death->getEntity()->getLastDamageCause()->getDamager();
-            if ($this->CheckIfPlayerHasBounty($player) === TRUE && $player instanceof Player && $killer instanceof Player){
+            if ($player instanceof Player){;
+            $killer = $death->getEntity()->getLastDamageCause()->getEntity();
+            //$this->getServer()->broadcastMessage($killer->getName());
+            if ($killer instanceof Player){
+            if ($this->CheckIfPlayerHasBounty($player) === TRUE){
+               //$cause = $death->getEntity()->getLastDamageCause()->getCause();
                     $yml = (new Config($this->getServer()->getDataPath() . "/plugins/Bounty/" . "Bounty.yml", Config::YAML ,array()));
                     $temp = $yml->getAll();
 
                     $message = $player->getName()."'s Bounty was collected by ".$killer->getName();
                     $this->getServer()->broadcastMessage($message);
                     $money = $this->PlayerBountyAmount($player);
+                    //$money = $temp['Current-Bounties'][$player->getName()]['bounty'];
                     $this->api->addMoney ( $killer->getName(), $money );
                     if ($temp['Death-Fine']){
                         $fee = ((($money)*1)*((($temp['Death-Fine'])*1)/100));
                         $force = true;
                         $this->api->reduceMoney($player->getName(), $fee, $force);
+                        //TAke Money From the User that Died
                     }
                     $this->RemovePlayerBounty($player);
+                    /*/$temp['Current-Bounties'][$player->getName()] = NULL;
+                    $yml->setAll($temp);
+                    $yml->save();
+                     */
             }
+        }}
         }
-        
         public function CheckIfPlayerHasBounty(Player $player){
             $playern = $player->getName();
             $playerresuts = $this->db->query("SELECT * FROM bounty WHERE player='$playern';");
@@ -164,15 +209,20 @@ class Main extends PluginBase implements Listener{
             $playerresuts = $this->db->query("SELECT COUNT(*) as count FROM bounty WHERE player='$playern';");
             $multis = $playerresuts->fetchArray();
             $multi = $multis['count'];
+            
+            //mysql_num_rows($result);
             if ($multi > 1){
                 $newval = 0;
                 for ($x=0;$x<=$multi;$x++){
                     $message = $x;
-                    $playerresuts = $this->db->query("SELECT * FROM bounty WHERE player='$playern' Limit 1,$x;");
+                    //$this->getServer()->broadcastMessage($message);
+                    //$playerresuts = array();
+                    $playerresuts/*[$x]*/ = $this->db->query("SELECT * FROM bounty WHERE player='$playern' Limit 1,$x;");
                     $result = $playerresuts->fetchArray(SQLITE3_ASSOC);
                     $newval = $newval + ($result['amount']*1);
                     $message=$newval;
                     return $newval;
+                    //$this->getServer()->broadcastMessage($message);
                 }
             }else{
                 $playerresuts = $this->db->query("SELECT * FROM bounty WHERE player='$playern';");
